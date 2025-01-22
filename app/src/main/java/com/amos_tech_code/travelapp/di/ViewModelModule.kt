b0a0e5682d2688d4708b758533d0693cb737c4b7
Amos-Tech-code <amosk51332@gmail.com>
2025-01-22 19:23:12 +0300

    Adding HomeScreen

--- app/src/main/java/com/amos_tech_code/travelapp/di/ViewModelModule.kt
+++
@@ -1,7 +0,0 @@
-package com.amos_tech_code.travelapp.di
-
-import org.koin.dsl.module
-
-val viewModelModule = module {
-
-}
---
+++ app/src/main/java/com/amos_tech_code/travelapp/ui/feature/login/LoginViewModel.kt
@@ -0,0 +1,4 @@
+package com.amos_tech_code.travelapp.ui.feature.login
+
+class LoginViewModel {
+}
